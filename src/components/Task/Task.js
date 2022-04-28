41ec81b2933bc513c80c1055b4e5d91c3b40fa4c
zhaisan <zhaisan.moon@gamil.com>
2022-04-28 10:02:58 +0600

    first commit

---
+++ src/components/Task/Task.js
@@ -0,0 +1,13 @@
+import "./Task.css";
+
+function Task() {
+  return (
+    <input
+      className='new-todo'
+      placeholder='What needs to be done?'
+      autofocus
+    />
+  );
+}
+
+export default Task;
